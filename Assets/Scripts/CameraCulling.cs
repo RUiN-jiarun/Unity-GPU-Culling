771289f134fbd02e9ae233ef94499e170cd0b60c
RUiN-jiarun <1052707477@qq.com>
2021-08-10 15:12:14 +0800

    8.10 Occlusion Culling

--- Assets/Scripts/CameraCulling.cs
+++ Assets/Scripts/CameraCulling.cs
@@ -4,17 +4,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[StructLayout(LayoutKind.Sequential)]
+public struct SortingData
+{
+    public uint drawCallInstanceIndex; // 1
+    public float distanceToCam;         // 2
+};
 
 [RequireComponent(typeof(Camera))]
 public class CameraCulling : MonoBehaviour
 {
     private new Camera camera;
-    public ComputeShader cullingComputeShader;
+    public ComputeShader frustumCullingComputeShader;
+    public ComputeShader occlusionCullingComputeShader;
     public Renderer[] staticRenderers;
-    private int kernel;
+    private int frustumKernel;
+    private int occlusionKernel;
     private uint threadSizeX;
     public Bounds[] staticRendererBounds;
     public ComputeBuffer boundBuffer;
+    public HZB hiZBuffer; 
 
     // 为什么要保证resultBuffer不被序列化呢？
 
@@ -25,6 +34,9 @@ public class CameraCulling : MonoBehaviour
     [NonSerialized]
     public Plane[] frustumPlanes;
     public ComputeBuffer planeBuffer;
+
+    [NonSerialized]
+    public ComputeBuffer m_instancesSortingData;
 
     private Vector3 prevPos;
     private Quaternion prevRot;
@@ -59,9 +71,9 @@ public class CameraCulling : MonoBehaviour
     private void Init()
     {
         // 使用FindKernel函数，用名字找到ComputeShader中定义的一个运算unit
-        kernel = cullingComputeShader.FindKernel("OverlapFrustumAndBounds");
+        frustumKernel = frustumCullingComputeShader.FindKernel("OverlapFrustumAndBounds");
 
-        if (kernel < 0)
+        if (frustumKernel < 0)
         {
             Debug.LogError("CameraCulling.Init >> frustum and bound kernel not exist..");
         }
@@ -70,7 +82,7 @@ public class CameraCulling : MonoBehaviour
         uint threadSizeZ;
 
         // 获取GPU线程的X/Y/Z
-        cullingComputeShader.GetKernelThreadGroupSizes(kernel, out threadSizeX, out threadSizeY, out threadSizeZ);
+        frustumCullingComputeShader.GetKernelThreadGroupSizes(frustumKernel, out threadSizeX, out threadSizeY, out threadSizeZ);
 
         if (staticRendererBounds == null)
             staticRendererBounds = Array.ConvertAll(staticRenderers, (renderer) => renderer.bounds);
@@ -93,12 +105,36 @@ public class CameraCulling : MonoBehaviour
 
         // 把脚本的输入和ComputeShader的输入关联在一起
         // 输入的长度是所有渲染体的数量
-        cullingComputeShader.SetInt("resultLength", staticRenderers.Length);
+        frustumCullingComputeShader.SetInt("resultLength", staticRenderers.Length);
 
         // 将ComputeBuffer关联到外部，给其它阶段的shader提供数据
-        cullingComputeShader.SetBuffer(kernel, "bounds", boundBuffer);
-        cullingComputeShader.SetBuffer(kernel, "planes", planeBuffer);
-        cullingComputeShader.SetBuffer(kernel, "results", resultBuffer);
+        frustumCullingComputeShader.SetBuffer(frustumKernel, "bounds", boundBuffer);
+        frustumCullingComputeShader.SetBuffer(frustumKernel, "planes", planeBuffer);
+        frustumCullingComputeShader.SetBuffer(frustumKernel, "results", resultBuffer);
+
+        // Occlusion Culling
+
+
+        occlusionKernel = occlusionCullingComputeShader.FindKernel("CSMain");
+
+        occlusionCullingComputeShader.SetBuffer(occlusionKernel, "bounds", boundBuffer);
+        occlusionCullingComputeShader.SetBuffer(occlusionKernel, "results", resultBuffer);
+
+        // List<SortingData> sortingData = new List<SortingData>();
+        // for (int i = 0; i < staticRenderers.Length; i++)
+        // {
+        //     sortingData.Add(new SortingData() {
+        //             drawCallInstanceIndex = (uint)0,
+        //             distanceToCam = Vector3.Distance(staticRendererBounds[i].center, m_camPosition)
+        //         });
+        // }
+
+        // m_instancesSortingData = new ComputeBuffer(staticRenderers.Length, Marshal.SizeOf(typeof(SortingData)));
+        // m_instancesSortingData.SetData(sortingData);
+        
+        // occlusionCullingComputeShader.SetBuffer(occlusionKernel, "_SortingData", m_instancesSortingData);
+        resultBuffer.SetData(cullingResults);
+        
 
     }
 
@@ -112,7 +148,22 @@ public class CameraCulling : MonoBehaviour
         uint dispatchXLength = length / threadSizeX + (uint)(((int)length % (int)threadSizeX > 0) ? 1 : 0);
 
         // 启动ComputeShader的运算unit
-        cullingComputeShader.Dispatch(kernel, (int)dispatchXLength, 1, 1);
+        // frustumCullingComputeShader.Dispatch(frustumKernel, (int)dispatchXLength, 1, 1);
+        // resultBuffer.GetData(cullingResults);
+        Matrix4x4 v = camera.worldToCameraMatrix;
+        Matrix4x4 p = camera.projectionMatrix;
+        Matrix4x4 m_MVP = p * v;
+        Vector3 m_camPosition = camera.transform.position;
+
+        occlusionCullingComputeShader.SetFloat("_ShadowDistance", QualitySettings.shadowDistance);
+        occlusionCullingComputeShader.SetMatrix("_UNITY_MATRIX_MVP", m_MVP);
+        occlusionCullingComputeShader.SetVector("_CamPosition", m_camPosition);
+
+        occlusionCullingComputeShader.SetVector("_HiZTextureSize", hiZBuffer.TextureSize);
+        occlusionCullingComputeShader.SetTexture(occlusionKernel, "_HiZMap", hiZBuffer.Texture);
+        
+
+        occlusionCullingComputeShader.Dispatch(occlusionKernel, (int)dispatchXLength, 1, 1);
         resultBuffer.GetData(cullingResults);
 
         // 根据resultBuffer的内容，确定是否渲染
